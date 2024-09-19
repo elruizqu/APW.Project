37d028d6fcd6413bce537324aa7fbc3edd10aaac
elruizqu <eruiz40639@ufide.ac.cr>
2024-09-18 21:11:48 -0600

    Primera clase

---
+++ APW/APW.Global/LinqProcessor.cs
@@ -0,0 +1,15 @@
+﻿using APW.Models;
+
+namespace APW.Global;
+
+public class LinqProcessor
+{
+    //items
+    public List<Car> Cars = [];
+
+    public List<string> TransformCars() 
+    {
+        return null;
+        //return Cars.Where(c => c)
+    }
+}
